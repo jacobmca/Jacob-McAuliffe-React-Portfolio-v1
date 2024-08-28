2c89d7c2fcd7c3c2a80110281e8815e9862a1e47
Jacob McAuliffe <64445624+jacobmca@users.noreply.github.com>
2024-08-27 20:37:52 -0700

    Added alts to images.

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -5,7 +5,7 @@ function AboutMe() {
         <div>
             <h1 className="about-me text-center mt-5 mb-4">About Me</h1>
             <div className="img-container">
-                <img src="./jacob-picture.jpg" className="headshot-pic img-fluid img-thumbnail" />
+                <img src="./jacob-picture.jpg" alt="headshot" className="headshot-pic img-fluid img-thumbnail" />
             </div>
             <p className="description text-center">Jacob McAuliffe is a programmer from Los Angeles CA. He began programming in 2019 and has since grown into a full-stack software developer. Jacob's skills include front end languages including JavaScript, React, JQuery, HTML and CSS, and back end technologies such as Node, SQL, MongoDB, and Express. He is currently completing UC Berkeley's Coding Bootcamp, and works as the Technology Coordinator for Disney Television Animation.</p>
         </div>
